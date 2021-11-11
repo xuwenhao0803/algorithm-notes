6c2f382e1aa884ec69acfe042647e4d3d45c99d2
徐文浩 <934367068@qq.com>
2021-11-11 09:13:07 +0800

    add:添加链表目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,6 +22,15 @@ module.exports = {
         ]
       },
       {
+        title: '链表',   // 必要的
+        path: '/LinkList',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: true, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/LinkList/移除链表元素.md',
+        ]
+      },
+      {
         title: '双指针',   // 必要的
         path: '/accumulate',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
         collapsable: true, // 可选的, 默认值是 true,
